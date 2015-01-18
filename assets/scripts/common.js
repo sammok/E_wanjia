acdb779febfebdd39e278d98ae6cb3144faae6ba
Qianglong Mo <sammok2003@hotmail.com>
2015-01-19 02:21:57 +0800

    Write comment components for productDetail page.

--- assets/scripts/common.js
+++ assets/scripts/common.js
@@ -144,7 +144,121 @@ $(function () {
     panel();
 
 
+    /* comment-img: img tab */
+    function commentImg (){
+        var mod = $('.comment-img'),
+                hdItem = mod.find('.comment-img-hd li'),
+            bdItem = mod.find('.comment-img-bd li'),
+            bdItemLen = bdItem.length,
+            prevBtn = mod.find('.prev'),
+            nextBtn = mod.find('.next');
 
+        hdItem.click(function (){
+            var index = $(this).index();
+
+            $(this).addClass('active')
+                .siblings('li').removeClass('active');
+
+            $(this).parents('.comment-img-hd')
+                .siblings('.comment-img-bd').find('li').removeClass('active')
+                .eq(index).addClass('active');
+        });
+
+        prevBtn.click(function (){
+            var nowItemIndex = $('.comment-img-hd li.active').index();
+
+            if(nowItemIndex > 0) {
+                hdItem.eq(nowItemIndex).removeClass('active')
+                    .prev('li').addClass('active');
+                bdItem.eq(nowItemIndex).removeClass('active')
+                    .prev('li').addClass('active');
+            } else {
+            }
+        });
+
+        nextBtn.click(function (){
+            var nowItemIndex = $('.comment-img-hd li.active').index();
+
+            if(nowItemIndex < bdItemLen-1) {
+                hdItem.eq(nowItemIndex).removeClass('active')
+                    .next('li').addClass('active');
+                bdItem.eq(nowItemIndex).removeClass('active')
+                    .next('li').addClass('active');
+            } else {
+            }
+        });
+
+    }
+    commentImg();
+
+    // upload images and preview
+    var uploadImgIndex = 0;
+    function upLoadImg (){
+        function change(obj, img) {
+            var pic = $(img);
+            var file = $(obj);
+            var ext=file.val().substring(file.val().lastIndexOf(".")+1).toLowerCase();
+            // gif在IE浏览器暂时无法显示
+            if(ext!='png'&&ext!='jpg'&&ext!='jpeg'){
+                alert("文件必须为图片！"); return;
+            }
+            // IE浏览器
+            if (document.all) {
+                file.select();
+                file.blur();
+                var reallocalpath = document.selection.createRange().text;
+                var ie6 = /msie 6/i.test(navigator.userAgent);
+                // IE6浏览器设置img的src为本地路径可以直接显示图片
+                if (ie6) pic.src = reallocalpath;
+                else {
+                    /*这种方式不能控制大小*/
+                    // 非IE6版本的IE由于安全问题直接设置img的src无法显示本地图片，但是可以通过滤镜来实现
+                    //pic[0].style.filter = "progid:DXImageTransform.Microsoft.AlphaImageLoader(sizingMethod='image',src=\"" + reallocalpath + "\")";
+                    // 设置img的src为base64编码的透明图片 取消显示浏览器默认图片
+                    // pic[0].src = 'data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==';
+                    pic = pic.parent('div');
+                    pic.html('');
+                    pic[0].style.filter = "progid:DXImageTransform.Microsoft.AlphaImageLoader(sizingMethod=scale)";
+                    pic[0].filters.item("DXImageTransform.Microsoft.AlphaImageLoader").src = reallocalpath;
+                    pic[0].style.width = "100px";
+                    pic[0].style.height = "100px";
+                }
+            }else{
+                html5Reader(file, img);
+            }
+        }
+
+        function html5Reader(file, img){
+            var file = file[0].files[0];
+            var reader = new FileReader();
+            reader.readAsDataURL(file);
+            reader.onload = function(e){
+                var pic = $(img);
+                pic.attr("src", this.result);
+
+            }
+        }
+
+        // call
+        $('.chooseFile').change(function (){
+            var imgWrap = $(this).parents('.uploadImg');
+
+            // create img dom
+            imgWrap.find('.imgList')
+                .append('<span data-index="' + uploadImgIndex +'"><img/></span>');
+
+            // create img
+            change($(this), '.uploadImg span[data-index="' + uploadImgIndex +'"] img');
+
+            // add img src to input
+            imgWrap.find('input[type="hidden"]')
+                .eq(imgWrap.find('img').length-1).attr("value", imgWrap.find('.chooseFile').val());
+
+            // updata imgCount
+            imgWrap.find('.imgCount span').text(imgWrap.find('img').length);
+        });
+    }
+    upLoadImg();
     
 });
 
@@ -192,3 +306,6 @@ $.fn.extend({
         }, 1000);
     }
 });
+
+
+
